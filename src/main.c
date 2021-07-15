3c209ab893622c12ab8fdd72e05582d5aee5f4aa
onContentStop <mariorules24@gmail.com>
2021-07-15 14:31:08 -0500

    clean up

--- src/main.c
+++ src/main.c
@@ -2,6 +2,10 @@
 
 int main(int argc, char **argv)
 {
-    ConstructNfa("test set", 8, NULL);
+    nfa_t *nfa = ConstructNfa("test set", 8, NULL);
+    for (int i = 0; i < nfa->nodes.length; i++)
+    {
+        free(nfa->nodes.data[i]);
+    }
     return 0;
 }
