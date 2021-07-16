eaddf4f6d82e50f27d7f5f2de4b0c94c8e426eb2
onContentStop <mariorules24@gmail.com>
2021-07-15 22:48:09 -0500

    vendor gc-lib to build on Windows

--- src/nfa.h
+++ src/nfa.h
@@ -10,47 +10,42 @@
 #include <uthash.h>
 #include <vec.h>
 
-typedef enum
-{
-    EDGE_EMPTY = -3,
-    EDGE_CHARACTER_CLASS = -2,
-    EDGE_EPSILON = -1,
+typedef enum {
+  EDGE_EMPTY = -3,
+  EDGE_CHARACTER_CLASS = -2,
+  EDGE_EPSILON = -1,
 } edge_t;
 
-typedef enum
-{
-    ANCHOR_NONE = 0,
-    ANCHOR_LINE_START = 1 << 0,
-    ANCHOR_LINE_END = 1 << 1,
-    ANCHOR_BOTH = ANCHOR_LINE_START | ANCHOR_LINE_END,
+typedef enum {
+  ANCHOR_NONE = 0,
+  ANCHOR_LINE_START = 1 << 0,
+  ANCHOR_LINE_END = 1 << 1,
+  ANCHOR_BOTH = ANCHOR_LINE_START | ANCHOR_LINE_END,
 } anchor_t;
 
-typedef struct NFA
-{
-    char *acceptString;
-    struct NFA *next[2];
-    edge_t edge;
-    anchor_t anchor;
-    bitset_t *characterClass;
-    bool inverted;
-    size_t index;
+typedef struct NFA {
+  char *acceptString;
+  struct NFA *next[2];
+  edge_t edge;
+  anchor_t anchor;
+  bitset_t *characterClass;
+  bool inverted;
+  size_t index;
 } nfa_node_t;
 
 void NfaNodeInit(nfa_node_t *node);
 
 typedef vec_t(nfa_node_t *) vec_nfa_node_t;
 
-typedef struct
-{
-    vec_nfa_node_t nodes;
-    size_t start;
+typedef struct {
+  vec_nfa_node_t nodes;
+  size_t start;
 } nfa_t;
 
-typedef struct
-{
-    char *name;
-    char *definition;
-    UT_hash_handle hh;
+typedef struct {
+  char *name;
+  char *definition;
+  UT_hash_handle hh;
 } macro_t;
 
 nfa_t *ConstructNfa(const char *regex, size_t len, macro_t *macros);
---
+++ windows/threads.h
@@ -0,0 +1,6 @@
+#ifndef LEX_THREADS_H
+#define LEX_THREADS_H
+
+#define thread_local _Thread_local
+
+#endif // LEX_THREADS_H
